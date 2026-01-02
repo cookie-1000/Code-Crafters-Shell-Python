a2cb3cd8e3229162ffcbf425be90f477342e06cf
cookie-1000 <siddhmistry90@gmail.com>
2026-01-02 16:12:02 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,35 @@
+import os
 import sys
 
 
 def main():
+    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        sys.stdout.flush()
-        command = input().strip()
+        command = input()
         if command == "exit":
-            sys.exit()
-        elif command[:4]=="echo":
-            print (command[5:])
-        elif command[:4]=="type":
-            if command[5:] in ["echo","exit","type"]:
-                print (command[5:]+ " is a shell builtin")
+            exit()
+        elif command.startswith("echo"):
+            command = command.split("echo ", 1)[1]
+            print(command)
+        elif command.startswith("type"):
+            command = command.split("type ", 1)[1]
+            if command in ["echo", "exit", "type"]:
+                print(f"{command} is a shell builtin")
             else:
-                print (f"{command[5:]}: not found")
-                continue
-        else: 
+                dirs = os.getenv("PATH", "").split(os.pathsep)
+                found = False
+                for dir in dirs:
+                    path = os.path.join(dir, command)
+                    if os.path.isfile(path) and os.access(path, os.X_OK):
+                        print(f"{command} is {path}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{command}: not found")
+        else:
             print(f"{command}: command not found")
-    
-        
+
+
 if __name__ == "__main__":
     main()
