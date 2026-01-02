4530d544a6946dd4d6b4b46e021785eec35bf971
cookie-1000 <siddhmistry90@gmail.com>
2026-01-02 00:22:31 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = input()
-        print(f"{command}: command not found")
+        command = input().strip()
         if command == "exit":
             sys.exit()
+        else: 
+            print(f"{command}: command not found")
+        
 if __name__ == "__main__":
     main()
